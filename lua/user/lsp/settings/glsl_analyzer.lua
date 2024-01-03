aa4cad080e3eacd4b32df58118ec37e5ac862f5c
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2024-01-02 16:00:01 -0800

    Add comments and format

--- lua/user/lsp/settings/glsl_analyzer.lua
+++ lua/user/lsp/settings/glsl_analyzer.lua
@@ -1,15 +1,17 @@
 local filetypes = { "glsl", "vert", "tesc", "tese", "frag", "geom", "comp" }
 
 for _, ft in ipairs(filetypes) do
+    -- https://neovim.io/doc/user/lua.html#vim.filetype
     vim.filetype.add({
         pattern = {
-            ["(.+)(%." .. ft ..")"] = "glsl"
+            -- pattern must end with .<ft> where <ft> is one of the filetypes above
+            ["(.+)(%." .. ft .. ")"] = "glsl"
         }
     })
 end
 
 return {
-	filetypes = filetypes,
-	single_file_support = true,
-	capabilities = {},
+    filetypes = filetypes,
+    single_file_support = true,
+    capabilities = {},
 }
