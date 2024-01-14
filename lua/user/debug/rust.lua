48b78e9d22a621c5b751c8995afbb11c8e859afd
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2024-01-13 16:11:32 -0800

    Fix indent

--- lua/user/debug/rust.lua
+++ lua/user/debug/rust.lua
@@ -1,7 +1,7 @@
 local status_ok, dap = pcall(require, "dap")
 if not status_ok then
-	print("Failed to load dap")
-	return
+    print("Failed to load dap")
+    return
 end
 
 local cwd = vim.fn.getcwd()
