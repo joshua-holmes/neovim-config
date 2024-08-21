b7f1e6916dc5c97c12142402dab0b4673d99229f
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2024-08-20 18:40:22 -0700

    Swap keybind to exit toggleterm

--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -37,7 +37,7 @@ end
 
 function _G.set_terminal_keymaps()
     local opts = { noremap = true }
-    vim.api.nvim_buf_set_keymap(0, "t", "<esc><esc>", [[<C-\><C-n>]], opts)
+    vim.api.nvim_buf_set_keymap(0, "t", "<C-end>", [[<C-\><C-n>]], opts)
 end
 
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
