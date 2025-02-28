56cb93e8c12148bc9d89ba904be8075d5419d058
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2025-02-28 12:17:20 -0800

    Configure GPT with different model and more tokens

--- lua/user/chatgpt.lua
+++ lua/user/chatgpt.lua
@@ -6,5 +6,9 @@ end
 
 local home = vim.fn.expand("$HOME")
 chatgpt.setup({
-    api_key_cmd = "gpg --decrypt " .. home .. "/.secrets/openai_api_key.gpg"
+    api_key_cmd = "gpg --decrypt " .. home .. "/.secrets/openai_api_key.gpg",
+    openai_params = {
+        model = "gpt-4o-mini",
+        max_tokens = 1000,
+    }
 })
