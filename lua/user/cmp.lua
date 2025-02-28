810c5c6af2962c3ad0fe1a9ee00f553b49fba584
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2025-02-28 11:59:33 -0800

    Print missing messages when importing fails

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -1,10 +1,12 @@
 local cmp_status_ok, cmp = pcall(require, "cmp")
 if not cmp_status_ok then
+    print("Failed to load cmp")
     return
 end
 
 local snip_status_ok, luasnip = pcall(require, "luasnip")
 if not snip_status_ok then
+    print("Failed to load luasnip")
     return
 end
 
