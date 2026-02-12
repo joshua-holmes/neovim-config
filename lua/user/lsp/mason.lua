2c69ea61b12bcc3304ba6abea81f6b76514c95ea
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2026-02-12 11:45:01 -0800

    Fix lspconfig warning

--- lua/user/lsp/mason.lua
+++ lua/user/lsp/mason.lua
@@ -27,7 +27,7 @@ local servers = {
 }
 
 local servers_to_ensure_installed = {}
-local excluded_from_check = { -- add servers to exclude form mason's lspconfig compatibility check here
+local excluded_from_check = {
     "zls",
     "rust",
     "gdscript",
@@ -44,6 +44,7 @@ for _, s in pairs(servers) do
         table.insert(servers_to_ensure_installed, s)
     end
 end
+
 require("mason").setup({
     ui = {
         border = "none",
@@ -56,36 +57,30 @@ require("mason").setup({
     log_level = vim.log.levels.INFO,
     max_concurrent_installers = 4,
 })
+
 require("mason-lspconfig").setup({
     ensure_installed = servers_to_ensure_installed,
     automatic_installation = true,
-    automatic_enable = false
+    automatic_enable = false,
 })
 
-local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
-if not lspconfig_status_ok then
-    print("Failed to load lspconfig")
-    return
-end
-
-local opts = {}
+local servers_to_enable = {}
 
 for _, server in pairs(servers) do
-    opts = {
-        on_attach = require("user.lsp.handlers").on_attach,
-        capabilities = require("user.lsp.handlers").capabilities,
-    }
-
     server = vim.split(server, "@")[1]
 
     if server == "rust_analyzer" then
         goto continue
     end
 
+    local opts = {
+        on_attach = require("user.lsp.handlers").on_attach,
+        capabilities = require("user.lsp.handlers").capabilities,
+    }
+
     local require_ok, conf_opts = pcall(require, "user.lsp.settings." .. server)
     if require_ok then
         if server == "rust" then
-            -- merge on_attach
             local old_fn = conf_opts.server.on_attach
             conf_opts.server.on_attach = function(client, bufnr)
                 opts.on_attach(client, bufnr)
@@ -98,6 +93,10 @@ for _, server in pairs(servers) do
         opts = vim.tbl_deep_extend("force", conf_opts, opts)
     end
 
-    lspconfig[server].setup(opts)
+    vim.lsp.config(server, opts)
+    table.insert(servers_to_enable, server)
+
     ::continue::
 end
+
+vim.lsp.enable(servers_to_enable)
